b2b5f94d1d9aa7af3c6ee2af5ac1b319bc057d6c
YuLim2 <jyulim0120@gmail.com>
2021-11-10 00:04:59 +0900

    expo 통해 파일 받기

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="black" />
+      <View style={styles.city}> 
+        <Text style={styles.cityName}>Busan</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  //css라고 생각!
   container: {
+    flex: 1,  //flex
+    backgroundColor: "#58CCFF",
+  },
+  city: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#58CCFF",
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  weather: {
+    flex: 3,
+  },
+  cityName: {
+    fontSize: 45,
+    fontWeight: "500",
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 30,
+    fontSize: 150,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
+  }
+})
